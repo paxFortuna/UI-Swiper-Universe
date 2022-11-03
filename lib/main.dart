2019d0063cf07819ac6ab60e627de39f88576eb3
fortuna <avidia38@gmail.com>
2022-11-03 19:33:56 +0900

    issue: non_nullsafety in swifer

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:ui_swiper_universe/screens/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
